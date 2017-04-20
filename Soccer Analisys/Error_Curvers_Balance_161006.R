1ae2c07fb778aa8b4c25edcab5c9ccb918714dad
wellrangel <wellrangel@gmail.com>
2017-04-20 15:26:12 -0300

    Init

---
+++ Soccer Analisys/Error_Curvers_Balance_161006.R
@@ -0,0 +1,107 @@
+library(ggplot2)
+
+Balance <- read.csv(file.choose(), sep=";")
+
+head(Balance)
+
+
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval=.95, .drop=TRUE) {
+  library(plyr)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = mean   (xx[[col]], na.rm=na.rm),
+                     sd   = sd     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+  
+  # Rename the "mean" column    
+  datac <- rename(datac, c("mean" = measurevar))
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
+
+
+
+
+#summary(Balance/Unbalance)
+CS = summarySE(Balance, measurevar = "Goal.potential", groupvars=c("Situation.type", "State"))
+
+CS
+# The errorbars overlapped, so use position_dodge to move them horizontally
+pd <- position_dodge(0.1) # move them .05 to the left and right
+
+
+plot.title = "Balance:save in green; goal in red"
+plot.subtitle = "Goal potential dynamics"
+
+ggplot(CS, aes(x=State, y=Goal.potential, colour=Situation.type)) + 
+  geom_errorbar(aes(ymin=Goal.potential-se, ymax=Goal.potential+se), width=.1, position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd, size=3, shape=21, fill="white") +
+  ggtitle(bquote(atop(.(plot.title), atop(italic(.(plot.subtitle)), "")))) +  #format title and subtitle
+  expand_limits(y=0) +                        # Expand y range
+  theme_bw() +
+  theme(axis.text.x = element_text(size=16), axis.text.y = element_text(size=16)) + #font axis x
+  theme(axis.title.x = element_text(size=20), axis.title.y = element_text(size=20)) + #font title axis x
+  theme(legend.title = element_text(colour="blue", size=16, face="bold"), #font legend
+        text = element_text(colour="black", size=16), #font text legend
+        legend.justification=c(.5,0), #justification legend
+        legend.position=c(.5,0))      # Position legend in bottom center
+
+
+  
+  
+
+# Black error bars - notice the mapping of 'group=supp' -- without it, the error
+# bars won't be dodged!
+#ggplot(tgc, aes(x=dose, y=len, colour=supp, group=supp)) + 
+# geom_errorbar(aes(ymin=len-ci, ymax=len+ci), colour="black", width=.1, position=pd) +
+#  geom_line(position=pd) +
+#  geom_point(position=pd, size=3)
+
+#ggplot(tgc, aes(x=dose, y=len, colour=supp, group=supp)) + 
+ # geom_errorbar(aes(ymin=len-se, ymax=len+se), colour="black", width=.1, position=pd) +
+  #geom_line(position=pd) +
+  #geom_point(position=pd, size=3, shape=21, fill="white") + # 21 is filled circle
+  #xlab("Dose (mg)") +
+  #ylab("Tooth length") +
+  #scale_colour_hue(name="Supplement type",    # Legend label, use darker colors
+   #                breaks=c("OJ", "VC"),
+    #               labels=c("Orange juice", "Ascorbic acid"),
+     #              l=40) +                    # Use darker colors, lightness=40
+  #ggtitle("The Effect of Vitamin C on\nTooth Growth in Guinea Pigs") +
+  #expand_limits(y=0) +                        # Expand y range
+  #scale_y_continuous(breaks=0:20*4) +         # Set tick every 4
+  #theme_bw() +
+  #theme(legend.justification=c(1,0),
+   #     legend.position=c(1,0))               # Position legend in bottom right
+
+
+#CS
+#plot(CS)
+# Standard error of the mean
+#ggplot(tgc, aes(x=dose, y=len, colour=supp)) + 
+# geom_errorbar(aes(ymin=len-se, ymax=len+se), width=.1) +
+#  geom_line() +
+#  geom_point()
---
+++ Soccer Analisys/graficos/bc.R
@@ -0,0 +1,93 @@
+
+# plotly library
+library(plotly)
+
+bra_per = read.csv("Brasil Jogo 1 x Peru.csv", sep = ";")
+bra_per <- bra_per[c("SDO")]
+bra_per[is.na(bra_per)] <- 0
+bra_per$symbol = 0
+
+if (nrow(bra_per[bra_per$SDO == "CB", ]) > 0){
+  bra_per[bra_per$SDO == "CB", ]$symbol = 1
+}
+if (nrow(bra_per[bra_per$SDO == "CD", ]) > 0){
+  bra_per[bra_per$SDO == "CD", ]$symbol = 2
+}
+if (nrow(bra_per[bra_per$SDO == "CM", ]) > 0){
+  bra_per[bra_per$SDO == "CM", ]$symbol = 3
+}
+if (nrow(bra_per[bra_per$SDO == "F", ]) > 0){
+  bra_per[bra_per$SDO == "F", ]$symbol = 4
+}
+if (nrow(bra_per[bra_per$SDO == "PB", ]) > 0){
+  bra_per[bra_per$SDO == "PB", ]$symbol = 5
+}
+if (nrow(bra_per[bra_per$SDO == "PD", ]) > 0){
+  bra_per[bra_per$SDO == "PD", ]$symbol = 6
+}
+if (nrow(bra_per[bra_per$SDO == "PM", ]) > 0){
+  bra_per[bra_per$SDO == "PM", ]$symbol = 7
+}
+
+matrix <- data.frame(
+  Var1=integer(),
+  Var2=integer(),
+  sdo=character(),
+  symbol=integer(),
+  stringsAsFactors=FALSE)
+
+
+indiceX = 1
+indiceY = 0
+
+iniciou = FALSE
+for(i in 1:nrow(bra_per)) {
+  myRow <- matrix[1,]
+  rowAtual <- bra_per[i,]
+  
+  if (rowAtual$symbol > 0){
+      iniciou = TRUE
+      indiceY = indiceY + 1
+    
+    myRow$Var1 = indiceX
+    myRow$Var2 = indiceY
+    myRow$sdo = rowAtual$SDO
+    myRow$symbol = rowAtual$symbol
+  
+    matrix <- rbind(matrix, myRow)   
+  }else{
+    if (iniciou){
+      indiceX = indiceX + 1
+      indiceY = 0
+    }
+  }
+  
+}
+
+l <- list(
+  font = list(
+    family = "sans-serif",
+    size = 12,
+    color = "#000"),
+  bgcolor = "#E2E2E2",
+  bordercolor = "#FFFFFF",
+  borderwidth = 2)
+
+# Make the graph
+my_graph=plot_ly(matrix , x=~Var2 , y=~Var1 , 
+                 type="scatter", mode="markers+text" , hoverinfo="text" , 
+                 text=~sdo , textposition = "bottom center",
+                 marker=list(symbol=~symbol , 
+                size=15 , color="red" , opacity=0.5) )%>% 
+
+  layout(
+    title= "Brasil x Peru",
+    hovermode="closest",
+    yaxis = list(title="Poss number", range = matrix$Var1, dtick = 1),
+    #yaxis=list(autorange="reversed" , title="Poss number" , tickfont=list(color="white"), range = c(2, 5) ) ,
+    xaxis=list( title="SDO Sequence" , tickfont=list(color="white")),
+    legend = l
+  )
+
+# Show graph
+my_graph
